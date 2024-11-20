f0b1e643ac8a80986c2401950f2ccb3b0770d494
Siong Ann Tan <stan83@wisc.edu>
2024-11-19 22:22:32 -0600

    Removed dummy files

--- xv6-public/ide.d
+++
@@ -1,3 +0,0 @@
-ide.o: ide.c /usr/include/stdc-predef.h types.h defs.h param.h \
- memlayout.h mmu.h proc.h wmap.h x86.h traps.h spinlock.h sleeplock.h \
- fs.h buf.h
--- xv6-public/pipe.d
+++
@@ -1,2 +0,0 @@
-pipe.o: pipe.c /usr/include/stdc-predef.h types.h defs.h param.h mmu.h \
- proc.h wmap.h fs.h spinlock.h sleeplock.h file.h
